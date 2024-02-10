227b41800cf68507682b5e9bd00a235866726294
lentils <vancekuikahi@gmail.com>
2024-02-09 18:00:34 -0800

    added getters and setters

--- classes/items/equippable.py
+++ classes/items/equippable.py
@@ -15,3 +15,17 @@ class Equippable(Item):
         self.__damage = damage
     
     
+    ##### Getters and setters
+    @property
+    def durability(self) -> int:
+        return self.__durability
+    @durability.setter
+    def durability(self, durability_value) -> None:
+        self.__durability = durability_value
+    
+    @property
+    def damage(self) -> int:
+        return self.__damage
+    @damage.setter
+    def damage(self, damage_value) -> None:
+        self.__damage = damage_value
