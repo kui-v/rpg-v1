59a6c113f8c797974836bfdcd8b511d685cf5eff
lentils <vancekuikahi@gmail.com>
2024-02-09 17:43:52 -0800

    fixed inheritance

--- classes/items/equippable.py
+++ classes/items/equippable.py
@@ -1,19 +1,17 @@
-from item import Item
+from classes.items.item import Item
 
 class Equippable(Item):
     # Initializers
     def __init__(self, 
-                 name: str, 
+                 name: str,
                  description: str,
                  durability: int = 0,
                  damage: int = 0):
         super().__init__(
-            name, description
+            name, 'equippable', description
         )
         assert durability >= 0, f"{name} cannot have durability less than zero."
         self.__durability = durability
         self.__damage = damage
-
-
-    ##### Getters, setters, and printers
+    
     
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from classes.character import Character
-from classes.items.item import Item
+# from classes.character import Character
+# from classes.items.item import Item
+from classes.items.equippable import Equippable
 
-character_import_file = 'csv/characters.csv'
-Character.instantiate_from_csv(character_import_file)
-print(Character.all_characters)
+# character_import_file = 'csv/characters.csv'
+# Character.instantiate_from_csv(character_import_file)
+# print(Character.all_characters)
 
-item_import_file = 'csv/items.csv'
-Item.instantiate_from_csv(item_import_file)
-print(Item.all_items)
+# item_import_file = 'csv/items.csv'
+# Item.instantiate_from_csv(item_import_file)
+# print(Item.all_items)
 
-for item in Item.all_items:
-    print(item.name, item.item_type, item.description)
+# for item in Item.all_items:
+#     print(item.name, item.item_type, item.description)
+eqp = Equippable("dvd", "a dvd you can watch or throw", 5, 2)
+print(eqp.description)
