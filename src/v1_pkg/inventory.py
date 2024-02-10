5069087422baf985258fda9d797a74ba5bdb8ae7
lentils <vancekuikahi@gmail.com>
2024-02-10 00:45:17 -0800

    created inventory class

--- src/v1_pkg/inventory.py
+++ src/v1_pkg/inventory.py
@@ -1,5 +1,25 @@
 from item import Item
 
 class Inventory:
-    def __init__(self, capacity: float):
+    def __init__(self, 
+                 capacity: float, 
+                 inventory: list[Item] = []):
         self.__capacity = capacity
+        self.__inventory = inventory
+    
+
+    ##### Getters, setters, printers
+    @property
+    def capacity(self) -> float:
+        return self.__capacity
+    @capacity.setter
+    def capacity(self, capacity_value) -> None:
+        self.__capacity = capacity_value
+
+    @property
+    def inventory(self) -> list[Item]:
+        return self.__inventory
+    @inventory.setter
+    def inventory(self, inventory_value) -> None:
+        assert isinstance(inventory_value, list[Item]), f"Must be list of Items"
+        self.__inventory = inventory_value
