17c71e39031d15ce9ad937871b52bf3f4ed779d7
lentils <vancekuikahi@gmail.com>
2024-02-09 18:58:56 -0800

    added consumables class

---
+++ classes/items/consumable.py
@@ -0,0 +1,31 @@
+from classes.items.item import Item
+
+class Consumable(Item):
+    # Initializers
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 effect_type: str,
+                 effect_modifier: float):
+        super().__init__(
+            name, 'consumable', description
+        )
+        assert effect_modifier > 0, f"{name} must be a value greater than zero."
+        self.__effect_type = effect_type
+        self.__effect_modifier = effect_modifier
+
+
+    ##### Getters and setters
+    @property
+    def effect_type(self) -> str:
+        return self.__effect_type
+    @effect_type.setter
+    def effect_type(self, effect_type_value) -> None:
+        self.__effect_type = effect_type_value
+    
+    @property
+    def effect_modifer(self) -> float:
+        return self.__effect_modifier
+    @effect_modifer.setter
+    def effect_modifier(self, effect_modifier_value) -> None:
+        self.__effect_modifier = effect_modifier_value
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from classes.character import Character
 # from classes.items.item import Item
 from classes.items.equippable import Equippable
+from classes.items.consumable import Consumable
 
 # character_import_file = 'csv/characters.csv'
 # Character.instantiate_from_csv(character_import_file)
@@ -12,5 +13,8 @@ from classes.items.equippable import Equippable
 
 # for item in Item.all_items:
 #     print(item.name, item.item_type, item.description)
-eqp = Equippable("dvd", "a dvd you can watch or throw", 5, 2)
-print(eqp.description)
+eqp = Equippable(name="dvd", description="a dvd you can watch or throw", durability=5, damage=2)
+print(eqp.damage)
+
+con = Consumable(name="potion", description="increases health", effect_type="health_up", effect_modifier=.20)
+print(con.effect_type)
